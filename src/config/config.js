04d6f445c88c63a717761817644509aee1d6d837
欧阳龙蛟 <longjiao.ouyang@aorise.org>
2018-07-24 09:54:16 +0000

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -16,9 +16,9 @@ const oDomains = {
   domain: {
     // 本地配置 localhost:8902
     // test http://10.16.3.40:8080  dev http://10.16.3.40:8080
-    dev: 'http://localhost:8902',
-    test: 'http://10.16.3.40:8080',
-    prod: 'http://10.16.3.40:8080'
+    dev: 'http://10.16.3.40:8080',
+    test: 'http://10.16.4.18:8080',
+    prod: 'http://10.16.4.18:8080'
   },
   // 文件/图片/CDN地址
   fileDomain: {
