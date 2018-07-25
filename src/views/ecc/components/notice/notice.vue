e6e6e82932e8d26343f481c550b37022cb76a7cd
longjiao.ouyang <longjiao.ouyang@aorise.org>
2018-07-25 11:51:09 +0800

    应急指挥后台管理结构

---
+++ src/views/ecc/components/notice/notice.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>aafaew</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
